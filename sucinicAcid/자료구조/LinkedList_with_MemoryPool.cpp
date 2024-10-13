f8644e60f55edbac989eb6e512e5bb8be1fb04a7
sucinicAcid <81561044+sucinicAcid@users.noreply.github.com>
2024-10-13 13:09:29 +0900

    [feat] 메모리풀 링크드 리스트 삽입,삭제

--- sucinicAcid/자료구조/LinkedList_with_MemoryPool.cpp
+++ sucinicAcid/자료구조/LinkedList_with_MemoryPool.cpp
@@ -1,11 +1,14 @@
 /*
-기존의 링크드리스트 문제점: new 연산시 병목현상
+# 기존의 링크드리스트 문제점: new 연산시 병목현상
 
-해결방법: 메모리풀(전역 배열) 사용
+  해결방법: 메모리풀(전역 배열) 사용
 
-장점
-  1. 병목현상 (X)
-  2. 지울필요 없음
+  장점
+    1. 병목현상 (X)
+    2. 지울필요 없음
+
+
+# 삽입,삭제시 맨앞,맨뒤 예외처리 필요
 */
 
 #include <stdio.h>
@@ -43,6 +46,43 @@ void addNode2(int v)
 	head = myAlloc(v, head);
 }
 
+// 중간에 추가
+void insertNode(int tar, int newVal)
+{
+	if (head == NULL) return;
+
+	Node* p;
+	for (p = head; p != NULL; p = p->next) {
+		if (p->x == tar) break;
+	}
+
+	if (p == NULL) return;
+	p->next = myAlloc(newVal, p->next);
+}
+
+// 맨앞, 맨뒤 예외처리 필요
+void deleteNode(int tar)
+{
+	//0. tar이 맨 앞에 있는 경우
+	if (head == NULL) return;
+	if (head->x == tar) {
+		head = head->next;
+		return;
+	}
+
+	//1. tar 이전의 노드 찾기
+	Node* p;
+	for (p = head; p != NULL; p = p->next) {
+		if (p->next == NULL) return; // 마지막 Node 탐색시 tar이 존재하지 않는 경우
+		if (p->next->x == tar) {
+			break;
+		}
+	}
+
+	//2. tar 이전의 노드의 next를 변경
+	p->next = p->next->next;
+}
+
 int main(void)
 {
 	addNode(1);
@@ -53,6 +93,10 @@ int main(void)
 	addNode2(6);
 	addNode2(7);
 	addNode2(8);
+
+	deleteNode(3);
+	deleteNode(4);
+	deleteNode(8);
 
 	// 탐색
 	for (Node* p = head; p != NULL; p = p->next) {
